4fb1a665d6c17b862b4acf69edb18dd10da7ba41
nicklusk <fearofmusic@pm.me>
2026-08-03 15:04:30 -0400

    Fix: use default import syntax for NotificationListener

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react";
-import NotificationListener from "@/components/NotificationListener";
+import NotificationListener from "@/app/components/NotificationListener";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
